5c8144266ba200bc641ce21a02b8f9c1e7dfd7b0
shivisuper <shivank.shuman@gmail.com>
2023-12-08 22:27:58 +1100

    started day 3

---
+++ src/d3.rs
@@ -0,0 +1,32 @@
+#![allow(unused_variables)]
+
+use crate::utils;
+
+pub fn part1(input: String) -> i32 {
+    42
+}
+
+fn part1_sample() -> String {
+    String::from(
+        "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..",
+    )
+}
+
+#[test]
+fn test_part1() -> () {
+    let res = part1(part1_sample());
+    assert_eq!(4361, res);
+}
+
+pub fn part2(input: String) -> i32 {
+    42
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 mod d1;
 mod d2;
+mod d3;
 mod utils;
 
 use crate::utils::file::problem_input;
 fn main() {
     println!(
         "Part 1 solution is: {}",
-        d2::part1(problem_input("src/resources/d2_p1.txt"))
+        d3::part1(problem_input("src/resources/d3.txt"))
     );
     println!(
         "Part 2 solution is: {}",
-        d2::part2(problem_input("src/resources/d2_p1.txt"))
+        d3::part2(problem_input("src/resources/d3.txt"))
     );
 }
