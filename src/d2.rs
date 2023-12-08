d1fb8b393a01100010c32b33fd71ea17197ebb9e
Shivank Sharma <shivank@organicresponse.com>
2023-12-08 21:17:13 +1100

    start d2p2

--- src/d2.rs
+++ src/d2.rs
@@ -85,6 +85,22 @@ fn test_part1() -> () {
     assert_eq!(8, res);
 }
 
+#[test]
+fn test_part2() -> () {
+    let res = part2(part2_sample());
+    assert_eq!(2286, res);
+}
+
 pub fn part2(input: String) -> i32 {
     42
+}
+
+fn part2_sample() -> String {
+    String::from(
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    )
 }
