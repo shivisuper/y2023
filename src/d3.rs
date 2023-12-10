e15bceadd971f4ad34ed96e6621680691869427c
Shivank Sharma <shivank@organicresponse.com>
2023-12-10 22:13:44 +1100

    trying to write utility for transpose in rust

--- src/d3.rs
+++ src/d3.rs
@@ -25,6 +25,16 @@ fn test_part1() -> () {
     assert_eq!(4361, res);
 }
 
+#[test]
+fn transpose() {
+    let input: String = String::from("1,2,3,4\n5,6,7,8\n10,11,12,13\n");
+    let expected: String = String::from("1,5,10\n2,6,11\n3,7,12\n4,8,14\n");
+    let mut mat: Vec<Vec<u8>> = vec![];
+    input.lines().for_each(|line| {
+        mat.push(line.split(",").map(|s| s.parse::<u8>().unwrap()).collect());
+    });
+    println!("{:?}", mat);
+}
 pub fn part2(input: String) -> i32 {
     42
 }
